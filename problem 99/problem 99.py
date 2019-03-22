33f9eab500d12cc49f0ffa3cde97d2cb83d74a05
VasSider <48735603+VasSider@users.noreply.github.com>
2019-03-22 20:20:42 +0200

    Update problem 99.py

--- problem 99/problem 99.py
+++ problem 99/problem 99.py
@@ -4,13 +4,11 @@ import math
 #p099_base_exp.txt
 data = pd.read_csv("p099_base_exp.txt", sep=",", header=None) 
 
-#print(data.loc[0])
-
-_max_ = data[1].loc[0]*math.log10(data[0].loc[0])
+max_data = data[1].loc[0]*math.log10(data[0].loc[0])
 ls=[0]
 for i in range(0,len(data)):
-    if _max_ < data[1].loc[i]*math.log10(data[0].loc[i]):
-        _max_ = data[1].loc[i]*math.log10(data[0].loc[i])
+    if max_data < data[1].loc[i]*math.log10(data[0].loc[i]):
+        max_data = data[1].loc[i]*math.log10(data[0].loc[i])
         ls.append(i)
 
 print (ls[-1])
